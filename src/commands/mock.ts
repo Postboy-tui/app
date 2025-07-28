06ca46db9bba350f583a747630c494101c431879
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-07-28 17:52:13 +0530

    updated mock urls

--- src/commands/mock.ts
+++ src/commands/mock.ts
@@ -12,20 +12,60 @@ export async function mockApis(): Promise<void> {
 	const store: { url: string, method: string }[] = [{ url: 'https://fakestoreapi.com/products', method: 'GET' }]
 
 
-	const users: { url: string, method: string }[] = [{ url: 'https://jsonplaceholder.typicode.com/users', method: 'GET' }]
+// Mock API endpoints for various categories
+// (use let to avoid redeclaration on hot reloads)
+// Use globalThis to avoid redeclaration errors in hot reload/dev
+if (!(globalThis as any)._postboyMocks) {
+	(globalThis as any)._postboyMocks = {};
+}
+const _mocks = (globalThis as any)._postboyMocks;
 
-	const comments: { url: string, method: string }[] = [{ url: 'https://jsonplaceholder.typicode.com/comments', method: 'GET' }]
+_mocks.users = [
+	{ url: 'https://jsonplaceholder.typicode.com/users', method: 'GET' },
+	{ url: 'https://reqres.in/api/users', method: 'GET' },
+	{ url: 'https://randomuser.me/api/', method: 'GET' },
+];
 
+_mocks.comments = [
+	{ url: 'https://jsonplaceholder.typicode.com/comments', method: 'GET' },
+	{ url: 'https://jsonplaceholder.typicode.com/comments/1', method: 'GET' },
+];
 
-	const todos: { url: string, method: string }[] = [{ url: 'https://jsonplaceholder.typicode.com/todos', method: 'GET' }]
+_mocks.todos = [
+	{ url: 'https://jsonplaceholder.typicode.com/todos', method: 'GET' },
+	{ url: 'https://jsonplaceholder.typicode.com/todos/1', method: 'GET' },
+];
 
+_mocks.weather = [
+	{ url: 'https://api.open-meteo.com/v1/forecast?latitude=35&longitude=139&hourly=temperature_2m', method: 'GET' },
+	{ url: 'https://api.weatherapi.com/v1/current.json?key=demo&q=London', method: 'GET' },
+];
+
+_mocks.books = [
+	{ url: 'https://openlibrary.org/works/OL45883W.json', method: 'GET' },
+	{ url: 'https://gutendex.com/books', method: 'GET' },
+];
+
+_mocks.animals = [
+	{ url: 'https://dog.ceo/api/breeds/image/random', method: 'GET' },
+	{ url: 'https://catfact.ninja/fact', method: 'GET' },
+];
 
 	const answers = await inquirer.prompt([
 		{
 			type: 'list',
 			name: 'category',
 			message: chalk.green('Select categories :'),
-			choices: ['posts', 'ecommerce/store', 'users', 'comments', 'todos']
+			choices: [
+				'posts',
+				'ecommerce/store',
+				'users',
+				'comments',
+				'todos',
+				'weather',
+				'books',
+				'animals',
+			],
 		},
 
 		{
@@ -40,21 +80,16 @@ export async function mockApis(): Promise<void> {
 
 	try {
 		switch (answers.category) {
-			case 'posts': post.map(p => logger.info
-				(p.url) + '\n')
-				break
-			case 'ecommerce/store': store.map(s => logger.info
-				(s.url) + '\n')
-				break
-			case 'users': users.map(u => logger.info
-				(u.url) + '\n')
-				break
-			case 'comments': comments.map(c => logger.info
-				(c.url) + '\n')
-				break
-			case 'todos': todos.map(t => logger.info
-				(t.url) + '\n')
-				break
+			case 'posts': _mocks.post.map((p: any) => logger.info(p.url) + '\n'); break;
+			case 'ecommerce/store': _mocks.store.map((s: any) => logger.info(s.url) + '\n'); break;
+			case 'users': _mocks.users.map((u: any) => logger.info(u.url) + '\n'); break;
+			case 'comments': _mocks.comments.map((c: any) => logger.info(c.url) + '\n'); break;
+			case 'todos': _mocks.todos.map((t: any) => logger.info(t.url) + '\n'); break;
+			default:
+				if (answers.category === 'weather') _mocks.weather.map((w: any) => logger.info(w.url) + '\n');
+				else if (answers.category === 'books') _mocks.books.map((b: any) => logger.info(b.url) + '\n');
+				else if (answers.category === 'animals') _mocks.animals.map((a: any) => logger.info(a.url) + '\n');
+				break;
 		}
 		logger.warning('adjust id numbers in request url to avoid getting bulk responses')
 	} catch (err) {
