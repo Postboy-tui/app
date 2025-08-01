b08ebd7cfdcefc21c5f0e1925fb88d41592feeda
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-08-01 10:48:53 +0530

    updated colors]

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,8 +1,8 @@
-import type { ThemeColors } from "../types";
+import type { Theme } from "../types";
 
-export const getStatusColor = (status: string, theme: ThemeColors) => {
-  if (status.startsWith('2')) return theme.success;
-  if (status.startsWith('4')) return theme.error;
-  if (status.startsWith('5')) return theme.error;
-  return theme.accent;
+export const getStatusColor = (status: string, theme: Theme) => {
+	if (status.startsWith('2')) return theme.colors.success;
+	if (status.startsWith('4')) return theme.colors.error;
+	if (status.startsWith('5')) return theme.colors.error;
+	return theme.colors.accent;
 };
