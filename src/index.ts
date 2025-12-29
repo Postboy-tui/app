8fb6e7271800e1ddd6502859e0cb1b9e01156f7d
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-12-29 18:06:05 +0530

    updated version

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,32 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-import chalk from 'chalk';
-import { logger, TIP_MESSAGE } from './utils/logger';
-import { uiCommand } from './commands/ui.js';
-import { testCommand } from './commands/test';
-import { mockApis } from './commands/mock';
-
+import { Command } from "commander";
+import chalk from "chalk";
+import { logger, TIP_MESSAGE } from "./utils/logger";
+import { uiCommand } from "./commands/ui.js";
+import { testCommand } from "./commands/test";
+import { mockApis } from "./commands/mock";
 
 const program = new Command();
 
-
+program
+  .version("1.3.3")
+  .description(chalk.yellow("PostBoy CLI - Test your APIs with ease"));
 
 program
-	.version('1.3.2')
-	.description(chalk.yellow('PostBoy CLI - Test your APIs with ease'))
-
-
-program
-	.command('run')
-	.description('Run a test API request')
-	.action(testCommand);
+  .command("run")
+  .description("Run a test API request")
+  .action(testCommand);
 
 program
-	.command('mock-list')
-	.description('List the mock API servers')
-	.action(mockApis)
+  .command("mock-list")
+  .description("List the mock API servers")
+  .action(mockApis);
 
-program
-	.command('ui')
-	.description('UI for PostBoy')
-	.action(uiCommand)
-
+program.command("ui").description("UI for PostBoy").action(uiCommand);
 
 if (!process.argv.slice(2).length) {
-	logger.warning(TIP_MESSAGE);
+  logger.warning(TIP_MESSAGE);
 }
 
 program.parse(process.argv);
